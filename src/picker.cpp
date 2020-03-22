531c0ad7a4e92dd8a28958e21f7f7bd8dc676371
Daryl Xu <ziqiang_xu@yeah.net>
2020-03-23 00:11:45 +0800

    update the way of detect selection changing

--- src/picker.cpp
+++ src/picker.cpp
@@ -1,34 +1,40 @@
 #include "picker.h"
 #include <QApplication>
+#include <QByteArray>
 #include "defined.h"
 
 Picker::Picker()
 {
-    clipboard = QApplication::clipboard();
-    connect(clipboard, &QClipboard::selectionChanged,
-            this, [=] {
-        // do not emit the signal right now, for browser
-        text = clipboard->text(QClipboard::Selection);
-        if (!isPressed) emit wordsPicked(text);
-    });
+    process = new QProcess();
+
+    // 初始化Picker的时候保证pre_text和current_text一致，防止第一次按钮释放的时候：
+    // 出现pre_text为null，而current_text有值，触发wordsPicked信号
+    pre_text = getSelectedText();
+    current_text = pre_text;
 }
 
 void Picker::buttonPressed()
 {
-    this->text = "";
-    this->isPressed = true;
 }
 
+
+// 比对之前的选中文本和当前的文本，如果发生改变，则表示在进行文本选中操作，
+// 发送wordsPicked信号。
 void Picker::buttonReleased()
 {
-    this->isPressed = false;
-    // to get the selected text
-    INFO << "Text from picker" << text;
-    if (text != "") emit wordsPicked(text);
+    current_text = getSelectedText();
+    DEBUG << current_text << pre_text;
+    if (current_text != pre_text) {
+        emit wordsPicked(current_text);
+        pre_text = current_text;
+    }
 }
 
+
+// 使用xsel命令获取最近一次选中的文本
 QString Picker::getSelectedText()
-{
-    return clipboard->text(QClipboard::Selection);
-//    return "Picker";
+{    process->start("xsel");
+    process->waitForFinished();
+    QString selected_text = *new QString(process->readAllStandardOutput());
+    return selected_text;
 }
