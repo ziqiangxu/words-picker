bf3079e5f306af9aa5f9c539a387d5e64f961157
Daryl Xu <ziqiang_xu@yeah.net>
2020-04-27 19:17:02 +0800

    turn to QClipboard from xsel

--- src/picker.cpp
+++ src/picker.cpp
@@ -9,15 +9,23 @@ Picker::Picker()
     connect(clipboard, &QClipboard::selectionChanged,
             this, [=] {
         // do not emit the signal right now, for browser
+        // 浏览器不停地触发selection changed事件，导致大开销
         if (!isPressed) {
             current_text = clipboard->text(QClipboard::Selection);
             emit wordsPicked(current_text);
         }
     });
+
+    // text copyed
+//    connect(clipboard, &QClipboard::changed,
+//            this, [=] {
+//        emit wordsPicked(clipboard->text());
+//    });
 }
 
 void Picker::buttonPressed()
 {
+    DEBUG << "button pressed";
 }
 
 
@@ -25,8 +33,11 @@ void Picker::buttonPressed()
 // 发送wordsPicked信号。
 void Picker::buttonReleased()
 {
+    return;
+
+    // todo solve the xsel bug
     current_text = getSelectedText();
-    DEBUG << current_text << pre_text;
+    DEBUG << "button released:" << current_text << pre_text;
     if (current_text != pre_text) {
         emit wordsPicked(current_text);
         pre_text = current_text;
@@ -36,7 +47,10 @@ void Picker::buttonReleased()
 
 // 使用xsel命令获取最近一次选中的文本
 QString Picker::getSelectedText()
-{    process->start("xsel");
+{
+    process = new QProcess();
+//     在这里面启动它居然要开始死机了
+    process->start("/usr/bin/xsel");
     process->waitForFinished();
     QString selected_text = *new QString(process->readAllStandardOutput());
     return selected_text;
