c6cf8dda63072387c30424da7df79d95078493b7
Daryl.Xu <ziqiang_xu@qq.com>
2020-06-30 23:07:16 +0800

    update package script

--- package-deb.sh
+++ package-deb.sh
@@ -1,7 +1,7 @@
 # 用于自动打包成deb
 cd build
-cmake ../words-picker
+cmake ..
 make -j4
-cp words-picker ../deb/words-picker/opt/words-picker/words-picker
+cp src/words-picker ../deb/words-picker/opt/words-picker/words-picker
 cd ../deb
 dpkg -b words-picker words-picker.deb
